c766cc0da9b57013ece025272ab16c32c87ee20c
MuratCihanUludag <muratcihanuludag@hotmail.com>
2024-03-06 23:50:42 +0300

    Ana sayfa 3d component eklenmistir

--- Frontend/netflix-view/src/Pages/Home.jsx
+++ Frontend/netflix-view/src/Pages/Home.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Points from '../Components/ThreeComponents/Points'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+        <div className="position-ralative" style={{ height: "300vh" }}>
+            <div className="position-sticky top-0">
+                <Points />
+            </div>
+        </div>
+        <div className="container">
+            <div className="row point-row">
+                <div className="col-12">
+                    <h3 className="text-start mb-3" >İhtiyaçlarınıza Uygun Bir Plan</h3>
+                </div>                     
+            </div>
+        </div>
+    </div>
   )
 }
 
