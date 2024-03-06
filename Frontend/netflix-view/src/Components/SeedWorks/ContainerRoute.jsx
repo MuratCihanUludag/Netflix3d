2ebe7d596901c087cc7a15ea9731386a94e200bd
MuratCihanUludag <muratcihanuludag@hotmail.com>
2024-03-06 23:08:21 +0300

    React Router Dom ile Linkler olusturuld ve Rota yapilari verildi.

--- Frontend/netflix-view/src/Components/SeedWorks/ContainerRoute.jsx
+++ Frontend/netflix-view/src/Components/SeedWorks/ContainerRoute.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Home from "../../Pages/Home"
-import { Routes,Route } from 'react-router-dom'
+import About from '../../Pages/About'
+import { Route, Routes} from 'react-router-dom'
 function ContainerRoute() {
   return (
     <section>
         <Routes>
             <Route path='/' element={<Home/>} />
+            <Route path='/About' element={<About/>} />
         </Routes>
+
+    
     </section>
   )
 }
