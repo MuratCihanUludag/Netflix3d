f0da1aced78540e26f2ed528dda831211d84fc85
MuratCihanUludag <muratcihanuludag@hotmail.com>
2024-03-06 23:50:18 +0300

    Bootstrap.bundle.min.js eklenmistir.

--- Frontend/netflix-view/src/App.jsx
+++ Frontend/netflix-view/src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from './Components/SeedWorks/Navbar'
 import Footer from './Components/SeedWorks/Footer'
 import ContainerRoute from './Components/SeedWorks/ContainerRoute'
 import "./assets/css/App.css"
+import "./assets/js/bootstrap.bundle.min.js"
 function App() {
 
   return (
