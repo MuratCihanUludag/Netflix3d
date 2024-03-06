a1d57a4053453d3355573557378e523f21f9b4c5
MuratCihanUludag <muratcihanuludag@hotmail.com>
2024-03-06 12:07:36 +0300

    DTO eklenmistir. Amac PL BLL ve DAL kismin birbirinden iyicene ayristirip daha sade bir yapi elde etmektir.

---
+++ Backend/Netflix3dBackend/Netflix.DTO/Concrate/UserDTO.cs
@@ -0,0 +1,13 @@
+﻿using Netflix.DTO.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Netflix.DTO.Concrate
+{
+    internal class UserDTO : BaseDTO
+    {
+    }
+}
