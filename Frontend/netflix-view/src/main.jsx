0d32cde9a7882c45fe9180875ba0106cfc8b4685
MuratCihanUludag <muratcihanuludag@hotmail.com>
2024-03-18 16:59:19 +0300

    i18n coklu dil entegre edilmistir.

--- Frontend/netflix-view/src/main.jsx
+++ Frontend/netflix-view/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './assets/css/index.css'
 import { BrowserRouter } from 'react-router-dom'
+import i18n from './assets/js/i18n.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
