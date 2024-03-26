1fb43d1e60c74fe637a5347bc9339940f19989dd
MuratCihanUludag <muratcihanuludag@hotmail.com>
2024-03-26 20:54:47 +0300

    Domain katmanina baseEntity ve DataStatus eklenmistir.

---
+++ Backend/Netflix3dBackend/Netflix3d.Domain/Entities/Common/DataStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Netflix3d.Domain.Entities.Common
+{
+    public enum DataStatus
+    {
+        Insert,
+        Update,
+        Delete
+    }
+}
