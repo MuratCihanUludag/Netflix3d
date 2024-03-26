bd9abf7fea959aac84dd99c52bb2390047e1e319
MuratCihanUludag <muratcihanuludag@hotmail.com>
2024-03-26 20:55:25 +0300

    Proje referanslari verilmistir.

--- Backend/Netflix3dBackend/Netflix3d.API/Program.cs
+++ Backend/Netflix3dBackend/Netflix3d.API/Program.cs
@@ -1,11 +1,18 @@
+using Netflix3d.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
+builder.Services.AddPersistenceService();
+
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
 
 var app = builder.Build();
 
