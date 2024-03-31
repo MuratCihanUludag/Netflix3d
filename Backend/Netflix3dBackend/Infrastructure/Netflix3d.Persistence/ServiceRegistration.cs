4018e75ce924558c6fb24a7d4690b8a287c7c862
MuratCihanUludag <muratcihanuludag@hotmail.com>
2024-03-31 20:08:11 +0300

    Scope olarak AppUser ve AppRole eklendi

--- Backend/Netflix3dBackend/Infrastructure/Netflix3d.Persistence/ServiceRegistration.cs
+++ Backend/Netflix3dBackend/Infrastructure/Netflix3d.Persistence/ServiceRegistration.cs
@@ -1,7 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Netflix3d.Application.Repositories;
+using Netflix3d.Domain.Entities.Identity;
 using Netflix3d.Persistence.Context;
+using Netflix3d.Persistence.Repositories.AppRoleRepository;
+using Netflix3d.Persistence.Repositories.AppUserRepository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,6 +23,12 @@ namespace Netflix3d.Persistence
         public static void AddPersistenceService(this IServiceCollection services)
         {
             services.AddDbContext<NetflixDbContext>(opt => opt.UseSqlServer(Configuration.ConnectionString));
+            services.AddScoped<IReadRepository<AppUser>, AppUserReadRepository>();
+            services.AddScoped<IWriteRepositry<AppUser>, AppUserWriteRepository>();
+
+            services.AddScoped<IReadRepository<AppRole>, AppRoleReadRepository>();
+            services.AddScoped<IWriteRepositry<AppRole>, AppRoleWriteRepository>();
+
         }
     }
 }
