7c5e5338ecc4b6ac35f6235b48e2272ba85b95f4
MuratCihanUludag <muratcihanuludag@hotmail.com>
2024-03-31 20:06:44 +0300

    Kullanici ve rollerin yazma ve okuma repositoryleri olusturuldu,

---
+++ Backend/Netflix3dBackend/Core/Netflix3d.Application/Repositories/AppUser/IAppUserReadRepository.cs
@@ -0,0 +1,8 @@
+﻿using Netflix3d.Domain.Entities.Identity;
+
+namespace Netflix3d.Application.Repositories
+{
+    public interface IAppUserReadRepository : IReadRepository<AppUser>
+    {
+    }
+}
---
+++ Backend/Netflix3dBackend/Core/Netflix3d.Application/Repositories/AppUser/IAppUserWriteRepository.cs.cs
@@ -0,0 +1,9 @@
+﻿using Netflix3d.Domain.Entities.Identity;
+
+
+namespace Netflix3d.Application.Repositories
+{
+    public interface IAppUserWriteRepository : IWriteRepositry<AppUser>
+    {
+    }
+}
