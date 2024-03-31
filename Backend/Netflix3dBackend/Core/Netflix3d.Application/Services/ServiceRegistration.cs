6c35792f471c0e5e7a3a9f5e20f6abf2cbf559f8
MuratCihanUludag <muratcihanuludag@hotmail.com>
2024-03-31 20:05:55 +0300

    jwt token ayarlari ve mediator servisinin ios konteynari ayarlari yapildi

---
+++ Backend/Netflix3dBackend/Core/Netflix3d.Application/Services/ServiceRegistration.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Netflix3d.Application.Services
+{
+    public static class ServiceRegistration
+    {
+        public static void AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddMediatR(opt => opt.RegisterServicesFromAssemblies(typeof(ServiceRegistration).Assembly));
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateAudience = true,
+                    ValidateIssuer = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = Configuration.Token["Token:Issuer"],
+                    ValidAudience = Configuration.Token["Token:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration.Token["Token:SecurityKey"])),
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+        }
+    }
+}
