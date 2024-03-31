46f98d08f11f44f4a42ce647a08f7d35cb6266d1
MuratCihanUludag <muratcihanuludag@hotmail.com>
2024-03-31 20:08:52 +0300

    Login ve Register controllerlarin temelleri atildi ve basit testi yapildi

---
+++ Backend/Netflix3dBackend/Presentation/Netflix3d.API/Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Netflix3d.Application.Features.Mediator.Queries.AppUserQueries;
+using Netflix3d.Application.Tools;
+
+namespace Netflix3d.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public LoginController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(GetCheckAppUserQuery query)
+        {
+            var values = await _mediator.Send(query);
+            if (values.IsExist)
+            {
+                return Created("", JwtTokenGenerator.TokenGenerator(values));
+            }
+            else
+            {
+                return BadRequest();
+            }
+
+        }
+    }
+}
---
+++ Backend/Netflix3dBackend/Presentation/Netflix3d.API/Controllers/RegistersController.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Netflix3d.Application.Features.Mediator.Commands.AppUserCommand.cs;
+
+namespace Netflix3d.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public RegistersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(CreateAppUserCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok();
+        }
+    }
+}
