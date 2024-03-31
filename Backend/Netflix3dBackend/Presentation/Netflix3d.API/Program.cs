e2c82375f3ea8c58b3d15fcfad913b43dac1af31
MuratCihanUludag <muratcihanuludag@hotmail.com>
2024-03-31 20:09:06 +0300

    Settingsler eklendi

--- Backend/Netflix3dBackend/Presentation/Netflix3d.API/Program.cs
+++ Backend/Netflix3dBackend/Presentation/Netflix3d.API/Program.cs
@@ -1,9 +1,14 @@
 using Netflix3d.Persistence;
+using Netflix3d.Application;
+using Netflix3d.Application.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddPersistenceService();
+
+builder.Services.AddApplicationServices(builder.Configuration);
+
 
 builder.Services.AddCors();
 
@@ -13,6 +18,10 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(opt =>
+{
+    opt.AddDefaultPolicy(builder => builder.AllowAnyHeader().AllowAnyMethod().AllowAnyMethod());
+});
 
 var app = builder.Build();
 
@@ -25,10 +34,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseCors(builder =>
-{
-    builder.WithOrigins("").AllowAnyHeader();
-});
+app.UseCors();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
