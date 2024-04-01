d3ba8968fe0c2a202da6233fa26046ff28cb89c3
MuratCihanUludag <muratcihanuludag@hotmail.com>
2024-04-01 19:48:33 +0300

    Role atama islemi duzeltildi

--- Backend/Netflix3dBackend/Core/Netflix3d.Application/Features/Mediator/Handlers/AppUserHandlers/CreatAppUserCommandHandler.cs
+++ Backend/Netflix3dBackend/Core/Netflix3d.Application/Features/Mediator/Handlers/AppUserHandlers/CreatAppUserCommandHandler.cs
@@ -23,24 +23,16 @@ namespace Netflix3d.Application.Features.Mediator.Handlers.AppUserHandlers
 
         public async Task Handle(CreateAppUserCommand request, CancellationToken cancellationToken)
         {
-            var UserRoles = _readAppRoleRepositry.GetAll();
-            Guid roleId = new Guid();
-            foreach (var role in UserRoles)
-            {
-                if (role.RoleName == "User")
-                {
-                    roleId = role.Id;
-                }
-            }
+            var userRole = await _readAppRoleRepositry.GetSingleAsync(u => u.RoleName == "User");
+
             _ = await _writeAppUserRepositry.AddAsync(new AppUser
             {
                 Name = request.Name,
                 Surname = request.Surname,
                 Email = request.Email,
                 Password = request.Password,
-                AppRoleId = roleId
-            });
-
+                AppRoleId = userRole.Id
+            }); ;
         }
     }
 }
