6bfac272461479dc66b4dbca20155849415dd7e8
MuratCihanUludag <muratcihanuludag@hotmail.com>
2024-04-01 19:52:24 +0300

    Auth islemleri ana sayfasi hazirlandi

--- Frontend/netflix-view/src/Pages/Auth/Auth.jsx
+++ Frontend/netflix-view/src/Pages/Auth/Auth.jsx
@@ -1,14 +1,31 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Starts from "../../Components/ThreeComponents/Starts";
+import LoginForm from "../../Components/Forms/LoginForm";
+import RegisterForm from "../../Components/Forms/RegisterForm";
 function Auth() {
 
+    const [type, setType] = useState(true);
+    
+    const handleOnClick = (res) => {
+        console.log(res)
+        setType(res);
+}
 
-  return (
-    <div className="position-relative" style={{ margin: `86px 0` }}>
-        <div className="container">
+
+return (
+    <div>
+        <div className="auth-img"></div>
+      {/* <Starts /> */}
+        <div className="container position-relative" style={{ height: "100vh" }}>
             <div className="row">
-                <div className="col-md-6">Login ve sing In sayfasi olacak</div>
-                <div className="col-md-6">3d model gelecek</div>
+                <div className={`col-md-8 col-12 mx-auto py-5 auth-form ${type != true ? "singUp":"singIn"}`}>
+                    <div className="row">
+                        <div className="col-12 auth-content">
+                            <LoginForm handleOnClick={handleOnClick} />
+                            <RegisterForm handleOnClick={handleOnClick} />                            
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
