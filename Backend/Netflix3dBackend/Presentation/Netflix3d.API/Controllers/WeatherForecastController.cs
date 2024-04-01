061aefb46c65bf17c6a4843aba408429973f9d01
MuratCihanUludag <muratcihanuludag@hotmail.com>
2024-04-01 19:50:39 +0300

    Test amacli authorize eklendi

--- Backend/Netflix3dBackend/Presentation/Netflix3d.API/Controllers/WeatherForecastController.cs
+++ Backend/Netflix3dBackend/Presentation/Netflix3d.API/Controllers/WeatherForecastController.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Netflix3d.API.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
