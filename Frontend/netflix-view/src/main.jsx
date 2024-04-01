3e4698bf2175e8afbbf21399f568a4f0f22e237d
MuratCihanUludag <muratcihanuludag@hotmail.com>
2024-04-01 19:52:35 +0300

    Contex yapisi islendi

--- Frontend/netflix-view/src/main.jsx
+++ Frontend/netflix-view/src/main.jsx
@@ -4,11 +4,14 @@ import App from './App.jsx'
 import './assets/css/index.css'
 import { BrowserRouter } from 'react-router-dom'
 import i18n from './assets/js/i18n.js'
+import { ApiContextProvider } from './Context/ApiContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <BrowserRouter>
+        <ApiContextProvider>
             <App />
+        </ApiContextProvider>
         </BrowserRouter>
     </React.StrictMode>
 )
