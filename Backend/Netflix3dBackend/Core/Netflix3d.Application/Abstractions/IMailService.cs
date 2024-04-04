7c4c69e1f5b8c4bd5d60e4e3f6f565ce7b3df6a9
MuratCihanUludag <muratcihanuludag@hotmail.com>
2024-04-04 22:03:13 +0300

    Unit Of Work ve AutoMapperin interfaceleri eklendi

---
+++ Backend/Netflix3dBackend/Core/Netflix3d.Application/Abstractions/IMailService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Netflix3d.Application.Abstractions
+{
+    public interface IMailService
+    {
+        Task SendMailAsync(string to, string subject, string body, bool isHtml = true);
+        Task SendMailAsync(string[] to, string subject, string body, bool isHtml = true);
+    }
+}
---
+++ Backend/Netflix3dBackend/Core/Netflix3d.Application/Abstractions/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using Netflix3d.Application.Repositories;
+using Netflix3d.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Netflix3d.Application.Abstractions
+{
+    public interface IUnitOfWork : IAsyncDisposable
+    {
+        public IReadRepository<T> GetReadRepository<T>() where T : BaseEntity, new();
+        public IWriteRepository<T> GetWriteRepository<T>() where T : BaseEntity, new();
+        Task<int> SaveAsync();
+        int Save();
+
+    }
+}
