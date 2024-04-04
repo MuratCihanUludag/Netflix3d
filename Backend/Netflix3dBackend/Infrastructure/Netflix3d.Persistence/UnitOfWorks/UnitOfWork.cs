584193c5562ce7fe05a4f04ede355ba8cf4d5b04
MuratCihanUludag <muratcihanuludag@hotmail.com>
2024-04-04 22:08:00 +0300

    UnitOfWork Eklenmistir.

---
+++ Backend/Netflix3dBackend/Infrastructure/Netflix3d.Persistence/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using Netflix3d.Application.Abstractions;
+using Netflix3d.Application.Repositories;
+using Netflix3d.Domain.Entities.Common;
+using Netflix3d.Persistence.Context;
+using Netflix3d.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Netflix3d.Persistence.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly NetflixDbContext _context;
+
+        public UnitOfWork(NetflixDbContext context)
+        {
+            _context = context;
+        }
+        public async ValueTask DisposeAsync() => await _context.DisposeAsync();
+        public int Save() => _context.SaveChanges();
+
+
+        public Task<int> SaveAsync() => _context.SaveChangesAsync();
+
+        public IReadRepository<T> GetReadRepository<T>() where T : BaseEntity, new() => new ReadRepository<T>(_context);
+
+        public IWriteRepository<T> GetWriteRepository<T>() where T : BaseEntity, new() => new WriteRepository<T>(_context);
+
+
+    }
+}
