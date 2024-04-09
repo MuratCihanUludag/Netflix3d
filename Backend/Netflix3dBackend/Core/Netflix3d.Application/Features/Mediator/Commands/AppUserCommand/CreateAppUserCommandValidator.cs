f56893c7c56eb307c36e51ec15bf2da792c94be5
MuratCihanUludag <muratcihanuludag@hotmail.com>
2024-04-09 17:52:49 +0300

    PipleLine calismasi inicin commandlara unit eklendi

---
+++ Backend/Netflix3dBackend/Core/Netflix3d.Application/Features/Mediator/Commands/AppUserCommand/CreateAppUserCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Netflix3d.Application.Features.Mediator.Commands.AppUserCommand
+{
+    public class CreateAppUserCommandValidator : AbstractValidator<CreateAppUserCommand>
+    {
+        public CreateAppUserCommandValidator()
+        {
+            RuleFor(x => x.Email).NotEmpty();
+            RuleFor(x => x.Password).NotEmpty().MinimumLength(6).WithMessage("Min 6").Matches(@"[A-Z]+").WithMessage("1 Upper Case");
+        }
+    }
+}
