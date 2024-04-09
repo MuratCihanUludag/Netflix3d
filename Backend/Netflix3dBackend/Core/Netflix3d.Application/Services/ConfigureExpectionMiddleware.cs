54eff201aa25eda01cc49d9d6248cd9ddc0c2de3
MuratCihanUludag <muratcihanuludag@hotmail.com>
2024-04-09 16:18:51 +0300

    app.settings icin bir aplication katanina bir adet middelware containiri olusturuldu

---
+++ Backend/Netflix3dBackend/Core/Netflix3d.Application/Services/ConfigureExpectionMiddleware.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Netflix3d.Application.Expections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Netflix3d.Application.Services
+{
+    public static class ConfigureExpectionMiddleware
+    {
+        public static void ConfigureExpectionHandleingMiddleware(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<ExpectionMiddelware>();
+        }
+    }
+}
