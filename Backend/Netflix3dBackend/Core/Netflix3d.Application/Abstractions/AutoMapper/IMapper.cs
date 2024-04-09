02b1847f447036c8614aa39070ccec50f2b5f5ac
MuratCihanUludag <muratcihanuludag@hotmail.com>
2024-04-09 16:16:35 +0300

    Yazim Hatasi duzeltildi

--- Backend/Netflix3dBackend/Core/Netflix3d.Application/Abstractions/AutoMapper/IMapper.cs
+++ Backend/Netflix3dBackend/Core/Netflix3d.Application/Abstractions/AutoMapper/IMapper.cs
@@ -9,8 +9,7 @@ namespace Netflix3d.Application.Abstractions.AutoMapper
     public interface IMapper
     {
         TDestination Map<TDestination, TSource>(TSource source, string? ignore = null);
-        IList<TDestination> Map<TDestination, TSource>(IList<TSource> sources, string ignore = null);
-
+        IList<TDestination> Map<TDestination, TSource>(IList<TSource> sources, string? ignore = null);
         TDestination Map<TDestination>(object source, string? ignore = null);
         IList<TDestination> Map<TDestination>(IList<object> source, string? ignore = null);
 
