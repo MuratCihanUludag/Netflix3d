af967836d9f4c847fcc32a1ca0d66601853975d3
MuratCihanUludag <muratcihanuludag@hotmail.com>
2024-04-09 16:20:21 +0300

    Yazim hatasi duzeltildi

--- Backend/Netflix3dBackend/Infrastructure/Netflix3d.Persistence/UnitOfWorks/UnitOfWork.cs
+++ Backend/Netflix3dBackend/Infrastructure/Netflix3d.Persistence/UnitOfWorks/UnitOfWork.cs
@@ -22,13 +22,11 @@ namespace Netflix3d.Persistence.UnitOfWorks
         public async ValueTask DisposeAsync() => await _context.DisposeAsync();
         public int Save() => _context.SaveChanges();
 
-
         public Task<int> SaveAsync() => _context.SaveChangesAsync();
 
         public IReadRepository<T> GetReadRepository<T>() where T : BaseEntity, new() => new ReadRepository<T>(_context);
 
         public IWriteRepository<T> GetWriteRepository<T>() where T : BaseEntity, new() => new WriteRepository<T>(_context);
-
 
     }
 }
