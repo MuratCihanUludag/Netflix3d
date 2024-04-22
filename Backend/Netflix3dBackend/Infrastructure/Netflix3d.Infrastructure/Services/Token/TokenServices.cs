ee6ef530b780d9a675296a507e6a17d8e8cb3e1b
MuratCihanUludag <muratcihanuludag@hotmail.com>
2024-04-23 02:03:56 +0300

    Token servisi infrastructre katmanina tasindi ve bastan duzenlendi

---
+++ Backend/Netflix3dBackend/Infrastructure/Netflix3d.Infrastructure/Services/Token/TokenServices.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Netflix3d.Application.Abstractions;
+using Netflix3d.Application.Features.Mediator.Results.AppUserResults;
+using Netflix3d.Domain.Entities.Identity;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Netflix3d.Infrastructure.Services.Token
+{
+    public class TokenServices : ITokenServices
+    {
+        private readonly TokenSettings _tokenSettings;
+        private readonly UserManager<AppUser> _userManager;
+
+        public TokenServices(IOptions<TokenSettings> options, UserManager<AppUser> userManager)
+        {
+            _tokenSettings = options.Value;
+            _userManager = userManager;
+        }
+
+        public async Task<JwtSecurityToken> CreateToken(AppUser user, IList<string> roles)
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(JwtRegisteredClaimNames.Email,user.Email.ToString())
+            };
+
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_tokenSettings.Secret));
+            var token = new JwtSecurityToken(
+                issuer: _tokenSettings.Issuer,
+                audience: _tokenSettings.Audience,
+                expires: DateTime.Now.AddMinutes(_tokenSettings.TokenValidateInMinute),
+                claims: claims,
+                signingCredentials: new SigningCredentials(key, SecurityAlgorithms.HmacSha256)
+                );
+
+            await _userManager.AddClaimsAsync(user, claims);
+
+            return token;
+        }
+
+        public string GenerateRefreshToken()
+        {
+            var randomNumber = new byte[64];
+            using var rng = RandomNumberGenerator.Create();
+            rng.GetBytes(randomNumber);
+            return Convert.ToBase64String(randomNumber);
+        }
+
+        public ClaimsPrincipal? GetClaimsPrincipalFromExpiredToken(string token)
+        {
+            TokenValidationParameters tokenValidationParametres = new()
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_tokenSettings.Secret)),
+            };
+            JwtSecurityTokenHandler tokenHandler = new();
+            var principal = tokenHandler.ValidateToken(token, tokenValidationParametres, out SecurityToken securityToken);
+            if (securityToken is not JwtSecurityToken jwtSecurityToken ||
+                !jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase))
+                    throw new SecurityTokenException("Token Bulunamadi");
+            return principal;   
+        }
+    }
+}
