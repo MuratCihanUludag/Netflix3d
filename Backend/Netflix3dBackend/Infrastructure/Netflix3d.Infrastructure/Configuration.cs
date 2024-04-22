829c287edf6415ce043a78ca08ffe6129605fb12
MuratCihanUludag <muratcihanuludag@hotmail.com>
2024-04-23 02:04:33 +0300

    IOC Container yapisi duzenlendi

---
+++ Backend/Netflix3dBackend/Infrastructure/Netflix3d.Infrastructure/Configuration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Netflix3d.Infrastructure
+{
+    public static class Configuration
+    {
+        public static IConfigurationRoot Token
+        {
+            get
+            {
+                ConfigurationBuilder manager = new();
+                manager.SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../Netflix3d.API"));
+                manager.AddJsonFile("appsettings.json");
+                return manager.Build();
+            }
+        }
+    }
+}
--- Backend/Netflix3dBackend/Infrastructure/Netflix3d.Infrastructure/ServiceRegistration.cs
+++ Backend/Netflix3dBackend/Infrastructure/Netflix3d.Infrastructure/ServiceRegistration.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using Netflix3d.Application.Abstractions;
 using Netflix3d.Infrastructure.Services;
+using Netflix3d.Infrastructure.Services.Token;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +18,22 @@ namespace Netflix3d.Infrastructure
         public static void AddInfrastructureServices(this IServiceCollection services)
         {
             services.AddScoped<IMailService, MailService>();
+            services.AddTransient<ITokenServices, TokenServices>();
+            services.Configure<TokenSettings>(Configuration.Token.GetSection("Token"));
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateAudience = true,
+                    ValidateIssuer = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = Configuration.Token["Token:Issuer"],
+                    ValidAudience = Configuration.Token["Token:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration.Token["Token:SecurityKey"])),
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
         }
     }
 }
