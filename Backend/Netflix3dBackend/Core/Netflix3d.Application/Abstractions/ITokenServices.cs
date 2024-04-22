85b81d847eef33701842b18b40ee62aeb2a5b072
MuratCihanUludag <muratcihanuludag@hotmail.com>
2024-04-23 02:01:49 +0300

    IToken servisi eklendi

---
+++ Backend/Netflix3dBackend/Core/Netflix3d.Application/Abstractions/ITokenServices.cs
@@ -0,0 +1,19 @@
+﻿using Netflix3d.Application.Features.Mediator.Results.AppUserResults;
+using Netflix3d.Domain.Entities.Identity;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Netflix3d.Application.Abstractions
+{
+    public interface ITokenServices
+    {
+        Task<JwtSecurityToken> CreateToken(AppUser user, IList<string> roles);
+        string GenerateRefreshToken();
+        ClaimsPrincipal? GetClaimsPrincipalFromExpiredToken(string token);
+    }
+}
