d5ac0bb647c7e37cadebb645f10b4ef8dcfc331a
MuratCihanUludag <muratcihanuludag@hotmail.com>
2024-04-23 02:02:34 +0300

    CerateAppUserCommand e Telefon Numarasi eklendi

--- Backend/Netflix3dBackend/Core/Netflix3d.Application/Features/Mediator/Commands/AppUserCommand/CreateAppUserCommand.cs
+++ Backend/Netflix3dBackend/Core/Netflix3d.Application/Features/Mediator/Commands/AppUserCommand/CreateAppUserCommand.cs
@@ -13,5 +13,6 @@ namespace Netflix3d.Application.Features.Mediator.Commands.AppUserCommand
         public string Surname { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        public string PhoneNumber { get; set; }
     }
 }
