527fe6031974e283117845f08269b53112f130b2
MuratCihanUludag <muratcihanuludag@hotmail.com>
2024-04-29 22:27:17 +0300

    RefreshTokenQueries eklendi

--- Backend/Netflix3dBackend/Core/Netflix3d.Application/Features/Mediator/Queries/RefreshTokenQueries/RefreshTokenQueries.cs
+++ Backend/Netflix3dBackend/Core/Netflix3d.Application/Features/Mediator/Queries/RefreshTokenQueries/RefreshTokenQueries.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using Netflix3d.Application.Features.Mediator.Results.RefreshTokenResults;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Netflix3d.Application.Features.Mediator.Queries.RefreshTokenQueries
 {
-    public class RefreshTokenQueries : IRequest
+    public class RefreshTokenQueries : IRequest<RefreshTokenQueriesResult>
     {
         public string AccessToken { get; set; }
         public string RefreshToken { get; set; }
