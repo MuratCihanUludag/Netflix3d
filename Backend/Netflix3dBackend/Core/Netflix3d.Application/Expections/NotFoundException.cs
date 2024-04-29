2252bcef2a5f8e3e0908630e76cb8c4589510718
MuratCihanUludag <muratcihanuludag@hotmail.com>
2024-04-29 22:32:52 +0300

    NotFoundException

--- Backend/Netflix3dBackend/Core/Netflix3d.Application/Expections/NotFoundException.cs
+++ Backend/Netflix3dBackend/Core/Netflix3d.Application/Expections/NotFoundException.cs
@@ -10,6 +10,6 @@ namespace Netflix3d.Application.Expections
     {
         public NotFoundException() { }
         public NotFoundException(string message) : base(message) { }
-        public NotFoundException(string message, Exception innerException) : base(message, innerException) { }
+        public NotFoundException(string message, Exception innerException) : base(message, innerException){ }
     }
 }
