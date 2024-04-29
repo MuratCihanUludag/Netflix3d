2995f1a4d8daa72584236bdb7998c95a6778bb93
MuratCihanUludag <muratcihanuludag@hotmail.com>
2024-04-29 22:26:30 +0300

    Iisim degisikligi yapildi

---
+++ Backend/Netflix3dBackend/Core/Netflix3d.Application/Features/Mediator/Results/AppUserResults/LoginQueryResult.cs
@@ -0,0 +1,9 @@
+﻿namespace Netflix3d.Application.Features.Mediator.Results.AppUserResults
+{
+    public class LoginQueryResult
+    {
+        public string Token { get; set; }   
+        public string RefreshToken { get; set; }    
+        public DateTime Expiration { get; set; }    
+    }
+}
